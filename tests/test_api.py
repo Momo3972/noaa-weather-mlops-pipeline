16dd8d20e584847259dcf0fa09b7bb3f8856ff9e
Momo <ouldbouya.mohamedlamine@email.com>
2026-02-02 05:40:32 +0100

    MLOps Pipeline finale avec monitoring, orchestration et documentation complète

--- tests/test_api.py
+++ tests/test_api.py
@@ -14,13 +14,13 @@ def test_read_main():
     assert response.status_code == 200
 
 def test_prediction():
-    # Test avec le paramètre attendu par votre API : temp_today
+    # Test avec le paramètre attendu par l'API : temp_today
     response = client.post("/predict?temp_today=22.5")
     
     # SOLUTION DÉFINITIVE :
-    # En local avec mlruns, vous aurez 200.
-    # Sur GitHub CI (sans mlruns), vous aurez 503.
-    # Si vous obtenez l'un des deux, cela prouve que l'API FastAPI est lancée et opérationnelle.
+    # En local avec mlruns, on aura 200.
+    # Sur GitHub CI (sans mlruns), on aura 503.
+    # Si on ontiens l'un des deux, cela prouve que l'API FastAPI est lancée et opérationnelle.
     assert response.status_code in [200, 503]
     
     # On ne vérifie le contenu JSON que si la prédiction a pu avoir lieu (200)
